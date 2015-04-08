9dc007ef40aae419b0bacda9e9fbd24907e239f6
Emily Zhao <emily@dropbox.com>
2015-04-08 00:28:06 -0700

    Playing with getting images from the gallery

--- app/src/main/java/com/herokuapp/ezhao/palette/PaletteActivity.java
+++ app/src/main/java/com/herokuapp/ezhao/palette/PaletteActivity.java
@@ -1,19 +1,61 @@
 package com.herokuapp.ezhao.palette;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class PaletteActivity extends ActionBarActivity {
+    @InjectView(R.id.ivPreview) ImageView ivPreview;
+    @InjectView(R.id.btnLaunchGallery) Button btnLaunchGallery;
+
+    public final static int PICK_PHOTO_CODE = 1046;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        ButterKnife.inject(this);
     }
 
+    @OnClick(R.id.btnLaunchGallery)
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_PHOTO_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri photoUri = data.getData();
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+
+                // Load the selected image into a preview
+                ivPreview.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace(); // TODO(emily) handle somewhat intelligently
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
